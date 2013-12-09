68afaaa6eef21f1700e53e0118c26588d3d4130c
titovanton <mail@titovanton.com>
2013-12-10 00:24:04 +0600

    new settings changes

--- djangomixins/_settings/secret.py
+++ djangomixins/_settings/secret.py
@@ -3,6 +3,11 @@
 U must add it to .gitignore file
 '''
 
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+TEMPLATE_DEBUG = True
+
 # Make this unique, and don't share it with anybody.
 SECRET_KEY = 'secret'
 
