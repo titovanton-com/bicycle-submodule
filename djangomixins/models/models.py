e4826fe3a2f31ff596aafb7d45a60c28d6d4ab99
titovanton <mail@titovanton.com>
2013-12-11 07:44:42 +0600

    ordering

--- djangomixins/models/models.py
+++ djangomixins/models/models.py
@@ -244,6 +244,15 @@ class OrderedQuerySetMixin(object):
         return self.__order(super(OrderedQuerySetMixin, self).all(**kwargs))
 
 
+class OrderedQuerySetMixin2(object):
+
+    def filter(self, **kwargs):
+        return super(OrderedQuerySetMixin2, self).filter(**kwargs).order_by('order_by', '-pk')
+
+    def all(self, **kwargs):
+        return super(OrderedQuerySetMixin2, self).all(**kwargs).order_by('order_by', '-pk')
+
+
 class StandartQuerySet(OrderedQuerySetMixin, PublishedQuerySet):
 
     def published(self, **kwargs):
