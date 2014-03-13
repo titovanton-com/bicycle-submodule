6a9955891a91db706e4bf236faa336b94a0e1514
titovanton <mail@titovanton.com>
2014-03-14 03:34:14 +0600

    new app carousel

---
+++ carousel/admin.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+from django.contrib import admin
+from sorl.thumbnail.admin import AdminImageMixin
+
+from models import Carousel
+from models import CarouselImage
+
+
+class CarouselImageInline(AdminImageMixin, admin.StackedInline):
+    model = CarouselImage
+    fk_name = 'carousel'
+    sortable_field_name = 'position'
+    extra = 0
+
+
+class CarouselAdmin(AdminImageMixin, admin.ModelAdmin):
+    inlines = [CarouselImageInline, ]
+    list_display = ('title', 'img_count',)
+
+
+admin.site.register(Carousel, CarouselAdmin)
+admin.site.register(CarouselImage)
---
+++ carousel/models.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+from django.db import models
+from bicycle.djangomixins.models import SlugMixin
+from bicycle.djangomixins.models import ImageBase
+
+
+class Carousel(SlugMixin):
+
+    def img_count(self):
+        return self.carouselimage_set.count()
+    img_count.short_description = u'Число картинок'
+    img_count.allow_tags = True
+
+    def get_images(self):
+        return self.carouselimage_set.all()
+
+    class Meta(object):
+        verbose_name_plural = u'Карусели'
+
+
+class CarouselImage(ImageBase):
+    carousel = models.ForeignKey(Carousel, verbose_name=u'Карусель')
