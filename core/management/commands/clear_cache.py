22165fb3e3151ace5bffe4ef6d0ff8c3d2d16322
titovanton <mail@titovanton.com>
2016-02-04 12:11:46 +0000

    rm core

--- core/management/commands/clear_cache.py
+++
@@ -1,9 +0,0 @@
-# coding: UTF-8
-
-from django.core.management.base import BaseCommand
-from django.core.cache import cache
-
-
-class Command(BaseCommand):
-    def handle(self, *args, **options):
-        cache.clear()
